2ab9ab640f99eed2d0b5557bba0731f972581fa1
krujos <jkruck@pivotal.io>
2015-10-22 13:33:11 -0500

    Initial impl of deploy for scripted apps

---
+++ deploy.sh
@@ -0,0 +1,32 @@
+#!env bash
+for v in $CF_API $CF_USERNAME $CF_PASSWORD $CF_APPS_DOMAIN; do 
+	if [[ -z $v ]] ; then
+		>&2 echo "$v must be set!"
+	fi
+done
+
+cf api $CF_API --skip-ssl-validation
+cf auth $CF_USERNAME $CF_PASSWORD
+cf create-org scripttests
+cf create-space tests -o scripttests
+cf target -o scripttests -s tests 
+
+git clone https://github.com/pivotal-customer0/cf-hello-world-sample-apps
+
+cd cf-hello-world-sample-apps
+
+for d in php python node ruby ; do                                                                                                      $
+	(cd $d && cf push $d -m 256m)
+	if [ $? != 0 ]; then
+		>&2 echo "Failed to push $d to $CF_API"
+		exit 1
+	fi
+
+	curl $d.$CF_APPS_DOMAIN
+ 	if [ $? != 0 ]; then
+		>&2 echo "Failed find $d at $d.$CF_APPS_DOMAIN"
+		exit 1
+	fi
+done
+
+cf delete-org scripttests -f
